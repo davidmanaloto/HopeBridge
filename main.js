3c4134b32f436b7942ad4fa0c34f814a93b62595
edratam <151847033+edratam@users.noreply.github.com>
2025-02-17 15:35:06 +0800

    homepage but not yet done
    edraline

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
+// Open pop-up modal (Sign In / Sign Up)
 function openModal(modalId) {
     document.getElementById(modalId).style.display = "flex";
 }
 
+// Close modal
 function closeModal(modalId) {
     document.getElementById(modalId).style.display = "none";
 }
 
+// Switch between Sign In and Sign Up modals
 function switchModal(closeModalId, openModalId) {
     closeModal(closeModalId);
     openModal(openModalId);
 }
+
+// Close modal if clicking outside
 window.onclick = function(event) {
     if (event.target.classList.contains("modal")) {
         event.target.style.display = "none";
