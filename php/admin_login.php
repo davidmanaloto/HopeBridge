ae758c1a48790311f934511c124c410716704148
davidmanaloto <daub.manaloto.up@phinmaed.com>
2025-03-18 10:29:10 +0800

    style

--- php/admin_login.php
+++ php/admin_login.php
@@ -75,7 +75,7 @@ if (empty($errors)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HopeBridge Login</title>
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="../css/login.css">
 </head>
 <body>
     <div class="login-wrapper">
